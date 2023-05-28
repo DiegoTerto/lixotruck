e5f069c6032268d3a9cc8fda164610d4664a0d14
DiegoTerto <diegotsouza@outlook.com>
2023-05-28 05:20:00 -0300

    fixing monogodb connect in heroku

--- src/main/java/br/com/lixotruck/lixotruck/LixotruckApplication.java
+++ src/main/java/br/com/lixotruck/lixotruck/LixotruckApplication.java
@@ -1,12 +1,14 @@
 package br.com.lixotruck.lixotruck;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class LixotruckApplication{
 
 	public static void main(String[] args) {
