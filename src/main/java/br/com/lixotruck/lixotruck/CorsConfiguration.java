e633cc286678d52f26e978b8e2f9de667715dec8
DiegoTerto <diegotsouza@outlook.com>
2023-06-01 07:33:18 -0300

    Arrumando cors front-end

--- src/main/java/br/com/lixotruck/lixotruck/CorsConfiguration.java
+++ src/main/java/br/com/lixotruck/lixotruck/CorsConfiguration.java
@@ -11,6 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://127.0.0.1:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
